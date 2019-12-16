d28490672c3e90d0539848ba2418661956b46e4d
Niels <niels.poelder@gmail.com>
2019-12-16 12:44:04 +0100

    Clues added + other essential scripts

---
+++ Assets/Scripts/Game Essential Scripts/Clue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clue : MonoBehaviour
+{
+    public string title;
+    public string description;
+    
+    private ClueManager mainClueManager;
+
+    private void Awake()
+    {
+        mainClueManager = FindObjectOfType<ClueManager>();
+    }
+    public void Scanning()
+    {
+        mainClueManager.LoadClue(title, description);
+    }
+}
